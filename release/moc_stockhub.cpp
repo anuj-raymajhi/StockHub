a5742db76fcee6749efc36bc3a8f3f29ae89f987
anuj_raymajhi <87628435+anuj-raymajhi@users.noreply.github.com>
2023-03-29 23:00:53 +0545

    Add files via upload

---
+++ release/moc_stockhub.cpp
@@ -0,0 +1,251 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'stockhub.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.1.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../sqllite/stockhub/stockhub.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'stockhub.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.1.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_stockhub_t {
+    const uint offsetsAndSize[62];
+    char stringdata0[683];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_stockhub_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_stockhub_t qt_meta_stringdata_stockhub = {
+    {
+QT_MOC_LITERAL(0, 8), // "stockhub"
+QT_MOC_LITERAL(9, 22), // "on_indexButton_clicked"
+QT_MOC_LITERAL(32, 0), // ""
+QT_MOC_LITERAL(33, 20), // "on_subButton_clicked"
+QT_MOC_LITERAL(54, 21), // "on_portButton_clicked"
+QT_MOC_LITERAL(76, 22), // "on_learnbutton_clicked"
+QT_MOC_LITERAL(99, 21), // "on_infoButton_clicked"
+QT_MOC_LITERAL(121, 21), // "on_exitButton_clicked"
+QT_MOC_LITERAL(143, 23), // "on_backButton_5_clicked"
+QT_MOC_LITERAL(167, 23), // "on_backButton_1_clicked"
+QT_MOC_LITERAL(191, 23), // "on_backButton_2_clicked"
+QT_MOC_LITERAL(215, 23), // "on_backButton_3_clicked"
+QT_MOC_LITERAL(239, 23), // "on_backButton_4_clicked"
+QT_MOC_LITERAL(263, 24), // "on_connectDB_btn_clicked"
+QT_MOC_LITERAL(288, 23), // "on_searchButton_clicked"
+QT_MOC_LITERAL(312, 21), // "on_bankingBtn_clicked"
+QT_MOC_LITERAL(334, 23), // "on_insuranceBtn_clicked"
+QT_MOC_LITERAL(358, 20), // "on_hydropBtn_clicked"
+QT_MOC_LITERAL(379, 16), // "on_MFbtn_clicked"
+QT_MOC_LITERAL(396, 23), // "on_othersButton_clicked"
+QT_MOC_LITERAL(420, 23), // "on_updateButton_clicked"
+QT_MOC_LITERAL(444, 21), // "on_addcoy_btn_clicked"
+QT_MOC_LITERAL(466, 24), // "on_deletecoy_btn_clicked"
+QT_MOC_LITERAL(491, 24), // "on_updt_port_btn_clicked"
+QT_MOC_LITERAL(516, 24), // "on_addpushButton_clicked"
+QT_MOC_LITERAL(541, 21), // "on_viewButton_clicked"
+QT_MOC_LITERAL(563, 23), // "on_deleteButton_clicked"
+QT_MOC_LITERAL(587, 24), // "on_AbtNEP_button_clicked"
+QT_MOC_LITERAL(612, 23), // "on_NEPSE_button_clicked"
+QT_MOC_LITERAL(636, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(660, 22) // "on_Term_button_clicked"
+
+    },
+    "stockhub\0on_indexButton_clicked\0\0"
+    "on_subButton_clicked\0on_portButton_clicked\0"
+    "on_learnbutton_clicked\0on_infoButton_clicked\0"
+    "on_exitButton_clicked\0on_backButton_5_clicked\0"
+    "on_backButton_1_clicked\0on_backButton_2_clicked\0"
+    "on_backButton_3_clicked\0on_backButton_4_clicked\0"
+    "on_connectDB_btn_clicked\0"
+    "on_searchButton_clicked\0on_bankingBtn_clicked\0"
+    "on_insuranceBtn_clicked\0on_hydropBtn_clicked\0"
+    "on_MFbtn_clicked\0on_othersButton_clicked\0"
+    "on_updateButton_clicked\0on_addcoy_btn_clicked\0"
+    "on_deletecoy_btn_clicked\0"
+    "on_updt_port_btn_clicked\0"
+    "on_addpushButton_clicked\0on_viewButton_clicked\0"
+    "on_deleteButton_clicked\0"
+    "on_AbtNEP_button_clicked\0"
+    "on_NEPSE_button_clicked\0on_pushButton_3_clicked\0"
+    "on_Term_button_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_stockhub[] = {
+
+ // content:
+       9,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      29,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  188,    2, 0x08,    0 /* Private */,
+       3,    0,  189,    2, 0x08,    1 /* Private */,
+       4,    0,  190,    2, 0x08,    2 /* Private */,
+       5,    0,  191,    2, 0x08,    3 /* Private */,
+       6,    0,  192,    2, 0x08,    4 /* Private */,
+       7,    0,  193,    2, 0x08,    5 /* Private */,
+       8,    0,  194,    2, 0x08,    6 /* Private */,
+       9,    0,  195,    2, 0x08,    7 /* Private */,
+      10,    0,  196,    2, 0x08,    8 /* Private */,
+      11,    0,  197,    2, 0x08,    9 /* Private */,
+      12,    0,  198,    2, 0x08,   10 /* Private */,
+      13,    0,  199,    2, 0x08,   11 /* Private */,
+      14,    0,  200,    2, 0x08,   12 /* Private */,
+      15,    0,  201,    2, 0x08,   13 /* Private */,
+      16,    0,  202,    2, 0x08,   14 /* Private */,
+      17,    0,  203,    2, 0x08,   15 /* Private */,
+      18,    0,  204,    2, 0x08,   16 /* Private */,
+      19,    0,  205,    2, 0x08,   17 /* Private */,
+      20,    0,  206,    2, 0x08,   18 /* Private */,
+      21,    0,  207,    2, 0x08,   19 /* Private */,
+      22,    0,  208,    2, 0x08,   20 /* Private */,
+      23,    0,  209,    2, 0x08,   21 /* Private */,
+      24,    0,  210,    2, 0x08,   22 /* Private */,
+      25,    0,  211,    2, 0x08,   23 /* Private */,
+      26,    0,  212,    2, 0x08,   24 /* Private */,
+      27,    0,  213,    2, 0x08,   25 /* Private */,
+      28,    0,  214,    2, 0x08,   26 /* Private */,
+      29,    0,  215,    2, 0x08,   27 /* Private */,
+      30,    0,  216,    2, 0x08,   28 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void stockhub::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<stockhub *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_indexButton_clicked(); break;
+        case 1: _t->on_subButton_clicked(); break;
+        case 2: _t->on_portButton_clicked(); break;
+        case 3: _t->on_learnbutton_clicked(); break;
+        case 4: _t->on_infoButton_clicked(); break;
+        case 5: _t->on_exitButton_clicked(); break;
+        case 6: _t->on_backButton_5_clicked(); break;
+        case 7: _t->on_backButton_1_clicked(); break;
+        case 8: _t->on_backButton_2_clicked(); break;
+        case 9: _t->on_backButton_3_clicked(); break;
+        case 10: _t->on_backButton_4_clicked(); break;
+        case 11: _t->on_connectDB_btn_clicked(); break;
+        case 12: _t->on_searchButton_clicked(); break;
+        case 13: _t->on_bankingBtn_clicked(); break;
+        case 14: _t->on_insuranceBtn_clicked(); break;
+        case 15: _t->on_hydropBtn_clicked(); break;
+        case 16: _t->on_MFbtn_clicked(); break;
+        case 17: _t->on_othersButton_clicked(); break;
+        case 18: _t->on_updateButton_clicked(); break;
+        case 19: _t->on_addcoy_btn_clicked(); break;
+        case 20: _t->on_deletecoy_btn_clicked(); break;
+        case 21: _t->on_updt_port_btn_clicked(); break;
+        case 22: _t->on_addpushButton_clicked(); break;
+        case 23: _t->on_viewButton_clicked(); break;
+        case 24: _t->on_deleteButton_clicked(); break;
+        case 25: _t->on_AbtNEP_button_clicked(); break;
+        case 26: _t->on_NEPSE_button_clicked(); break;
+        case 27: _t->on_pushButton_3_clicked(); break;
+        case 28: _t->on_Term_button_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject stockhub::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_stockhub.offsetsAndSize,
+    qt_meta_data_stockhub,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_stockhub_t
+
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *stockhub::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *stockhub::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_stockhub.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int stockhub::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 29)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 29;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 29)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 29;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
